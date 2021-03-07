e91f38387a9616273084cbaf77329882480fdc3c
Slava <ucheba00@gmail.com>
2021-03-07 00:49:51 -0700

    render a list of dummy meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://images.unsplash.com/photo-1565349173908-1cf6bc9fd4ee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80",
+    address: "Some address 5/21 apt 3",
+    description: "Some descr",
+  },
+];
 
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
--- pages/news/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function news() {
-  return <h1>News Page</h1>;
-}
-
-export default news;

952c7bb744e5378de37c301ab45c55e07d499c5d
Slava <ucheba00@gmail.com>
2021-03-07 20:21:48 -0700

    add send post request to API to create a meetup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,7 +6,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8g0v.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a8g0v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     );
     const db = client.db();
 
